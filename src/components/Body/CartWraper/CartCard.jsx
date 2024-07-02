643555d35caaf0def8bf07b8224768f4f6afe5ed
Sai Tharun <128915167+saithaRun-max@users.noreply.github.com>
2024-07-02 13:05:03 +0530

    cart page developed

--- src/components/Body/CartWraper/CartCard.jsx
+++ src/components/Body/CartWraper/CartCard.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React from "react";
+import { MdDelete } from "react-icons/md";
+import { MdCurrencyRupee } from "react-icons/md";
 
 const CartCard = () => {
   return (
     <div>
-      <div className='h-[130px] mb-6 border rounded-md shadow-md'>
-        <div>
-         <img src="" alt="image" />
+      <div className="w-[490px] mx-[10px] mb-6 border rounded-md shadow-md flex p-2 align-middle justify-between">
+        <div className=" w-[150px]">
+          <img
+            src="https://b.zmtcdn.com/web/about/a7b0a36d5107f3590895981dab2eeac31563208212.jpeg?output-format=webp"
+            alt="image"
+            className="h-[] w-[130px] rounded-md"
+          />
         </div>
-        
-
+        <div className=" w-[160px] ">
+          <h4 className="text-lg font-semibold text-rose-900 ">
+            {"One8 Commune"}
+          </h4>
+          <p className="mt-2">{"North Indian"}</p>
+        </div>
+        <div className="h-20 w-20 rounded-b-md bg-red-600 text-white">
+          <h4 className="text-center font-semibold text-">Price</h4>
+          <div className="flex align-middle mx-auto mt-2 ml-4 ">
+            <MdCurrencyRupee className="inline-block mt-1" />{" "}
+            <h2 className="font-semibold">{"200"}</h2>
+          </div>
+        </div>
+        <div className="mt-6">
+          <MdDelete className="text-2xl cursor-pointer " />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartCard
+export default CartCard;
--- src/components/Body/CartWraper/CartContainer.jsx
+++ src/components/Body/CartWraper/CartContainer.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import CartCard from "./CartCard";
+import { RxCross2 } from "react-icons/rx";
 
 const CartContainer = () => {
   return (
-    <div className="w-[480px] h-screen m-2 mx-auto p-4  rounded-sm backdrop-blur-xl shadow-lg flex flex-col">
-      <div className="flex justify-between align-middle">
-        <h4 className="font-semibold text-lg">My Cart</h4>
+    <>
+      <div className="absolute -right-16 -top-2 w-[680px]">
+        <div className="mx-[10%] h-screen m-2 p-4  rounded-sm backdrop-blur-xl shadow-lg flex flex-col bg-gradient-to-r from-gray-500">
+          <div className="flex align-bottom justify-between">
+            <h4 className="font-semibold text-2xl text-slate-200">My Cart</h4>
+            <RxCross2 className="mt-2 text-xl cursor-pointer" />
+          </div>
+          <div className=" flex justify-between align-middle"></div>
+          <div className="mt-4">
+            <CartCard />
+            <CartCard />
+          </div>
+        </div>
       </div>
-      <div className="mt-4">
-      <CartCard />
-      <CartCard />
-      </div>
-
-   
-    </div>
+    </>
   );
 };
 
